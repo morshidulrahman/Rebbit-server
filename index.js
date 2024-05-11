6db674ed10c67ff42606a0f4789b0c7119ce2087
morshidulrahman <boydanger416@gmail.com>
2024-05-11 15:25:14 +0600

    mongodb connect successfully

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 const express = require("express");
 const PORT = process.env.PORT || 5000;
 const cors = require("cors");
+require("dotenv").config();
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.m73tovo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const dbConnect = async () => {
+  try {
+    client.connect();
+    console.log("Database Connected Successfully");
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
+};
+dbConnect();
+
+const Database = client.db("rebbitDb");
+const QueriesCollection = Database.collection("queries");
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/queries", async (req, res) => {
+  const result = await QueriesCollection.find().toArray();
+  res.json(result);
 });
 
 app.listen(PORT, () => {
