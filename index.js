c6a01333060e903164cf48a8458586616ab667b5
morshidulrahman <boydanger416@gmail.com>
2024-05-11 16:55:08 +0600

    jwt-added

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
-const PORT = process.env.PORT || 5000;
-const cors = require("cors");
-require("dotenv").config();
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cookieParser = require("cookie-parser");
 
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: ["http://localhost:5173", "http://localhost:5174"],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.m73tovo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -33,13 +40,41 @@ dbConnect();
 const Database = client.db("rebbitDb");
 const QueriesCollection = Database.collection("queries");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.post("/jwt", async (req, res) => {
+  const user = req.body;
+  const token = jwt.sign(user, process.env.jwt_web_token, { expiresIn: "1h" });
+
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    })
+    .send({
+      success: "true",
+    });
+});
+
+app.get("/logout", (req, res) => {
+  res
+    .clearCookie("token", {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      maxAge: 0,
+    })
+    .send({
+      success: "true",
+    });
 });
 
 app.get("/queries", async (req, res) => {
   const result = await QueriesCollection.find().toArray();
   res.json(result);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
